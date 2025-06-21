148a7a0cb7539a38b51615450a2b6daddfaeffa7
iwa999 <armatis@inbox.ru>
2025-06-22 00:39:09 +0500

    fix: mount faq block

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import HeroAIDiagnose from '@/components/HeroAIDiagnose';
 import CalculatorCost from '@/components/CalculatorCost';
+import FaqBlock       from '@/components/FaqBlock';
 
 export default function Home() {
   return (
     <>
       <HeroAIDiagnose />
       <CalculatorCost />
+      <FaqBlock />
     </>
   );
 }
